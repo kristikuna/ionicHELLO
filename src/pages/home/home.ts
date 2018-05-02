f90921341798d7e9473f5d66f172f7d77f1aec9f
kristibyrnes <kristibyrnes@icloud.com>
2018-05-02 15:46:28 -0400

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,12 +3,36 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  // templateUrl: 'home.html'
+  template: `<ion-header>
+  <ion-navbar>
+   <button ion-button icon-only menuToggle>
+      <ion-icon name="menu"></ion-icon>
+    </button>
+
+     <ion-title>
+        Ionic App
+     </ion-title>
+
+  </ion-navbar>
+</ion-header>
+
+<ion-content padding>
+  <p>Hello World</p>
+</ion-content>
+`
 })
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    @ViewChild('myNav') nav: NavController
+    public rootPage: any = TabsPage;
+  
+    // Wait for the components in MyApp's template to be initialized
+    // In this case, we are waiting for the Nav with reference variable of "#myNav"
+    ngOnInit() {
+       // Let's navigate from TabsPage to Page1
+       this.nav.push(Page1);
+    }
   }
 
-}
